40835c9c8c652ed46e5aa58a48255c8f5d1c7446
Kaitlin Swanson <kateswanson94@gmail.com>
2021-06-28 14:50:02 -0700

    city search working

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const locationInput = document.getElementById('locationInput')
+const locationInput = document.getElementById('locationInput');
+const submitButton = document.getElementById('submitButton');
 
+submitButton.addEventListener('click', () => {
+  chooseCity();
+});
+
+function chooseCity() {
+    let city = locationInput.value; 
+   // console.log(city);
 
 //write the functions that hit the API
-//function chooseLocation(city) { 
- //   let city = locationInput.value; 
-
-//}
 
 async function getWeather() { 
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Detroit&APPID=d6c8b5a48846624c112af61e9513bf90', {mode: 'cors'});
+
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=d6c8b5a48846624c112af61e9513bf90', {mode: 'cors'});
     const weatherData = await response.json();
         console.log(weatherData.name);
     }
     getWeather();
-
+}
 //writhe the functions that PROCESS the JSON data and return an object with the data we need
 
 //response.main.temp gives temp (in Kelvin)
